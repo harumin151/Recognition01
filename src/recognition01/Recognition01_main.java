57df9c54f33ff3c3279b597e57e3bbdd984a8b1a
harumin151 <j16015@sangi.jp>
2018-05-24 12:52:31 +0900

    yes

--- src/recognition01/Recognition01_main.java
+++ src/recognition01/Recognition01_main.java
@@ -2,7 +2,10 @@ package recognition01;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
@@ -12,7 +15,7 @@ public class Recognition01_main {
 	public static void main(String[] args){
 
 	VisualRecognition service = new VisualRecognition("2018-03-19");
-	service.setApiKey("j16015");
+	service.setApiKey("e04ca9963f0798e059d0de9dc5a296c29ef447a6");
 
 	DetectFacesOptions detectFacesOptions = null;
 	try {
@@ -25,6 +28,49 @@ public class Recognition01_main {
 	}
 	DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
 	System.out.println(result);
-}
 
+
+	//　ココから第２回授業だお↓
+	String s = String.valueOf(result);
+	ObjectMapper mapper = new ObjectMapper();
+	JsonNode node = null;
+	try {
+		node = mapper.readTree(s);
+	} catch (IOException e) {
+		// TODO 自動生成された catch ブロック
+		e.printStackTrace();
+	}
+
+	//age_minを取得する
+	int age_min =
+			node.get("images").get(0).get("faces").get(0)
+.get("age").get("min").asInt();
+	System.out.println("age_min:" + age_min);
+
+	//age_maxを取得する
+	int age_max =
+			node.get("images").get(0).get("faces").get(0)
+			.get("age").get("max").asInt();
+	System.out.println("age_max:" + age_max);
+
+	//age_scoreを取得する
+	double age_score =
+			node.get("images").get(0).get("faces").get(0)
+			.get("age").get("score").asInt();
+	System.out.println("age_score:" + age_score);
+
+	//Genderを取得する
+	int gender =
+			node.get("images").get(0).get("faces").get(0)
+			.get("gender").get("gender").asInt();
+	System.out.println("Gender:" + gender);
+
+	//Gender_scoreを取得する
+	double gender_score =
+			node.get("images").get(0).get("faces").get(0)
+			.get("gender").get("score").asInt();
+	System.out.println("Gender_score:" + gender_score);
+
+
+	}
 }
